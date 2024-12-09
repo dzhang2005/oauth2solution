dbebedcc6b2d3058af694c6685c9507db8fc00eb
dzhang2005 <david_zhang_2000@hotmail.com>
2024-12-09 16:47:04 -0500

    initial checkin

---
+++ oauth2-server/src/test/java/com/davidzhang/consulting/oauth/server/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidzhang.consulting.oauth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
